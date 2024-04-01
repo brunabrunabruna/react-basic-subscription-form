dc68920274ff6daa203438baf2ce626da0287861
brunabrunabruna <brunaandreis@hotmail.com>
2024-04-01 16:06:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className=" bg-cyan-800 h-screen text-center flex min-h-full flex-col justify-center items-center">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
